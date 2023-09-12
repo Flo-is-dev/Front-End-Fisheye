92155d2a3a853959d07000448bde5197376a6bf5
Flo-le-dev <f.gironde.dev@gmail.com>
2023-09-12 19:21:15 +0200

    P6-

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -8,14 +8,43 @@ async function getPhotographers() {
 
     console.log(responseJS, "objet javascript");
 
-    // main.innerHTML = `<h1> ${responseJS.photographers[0].portrait} <br>${responseJS.photographers[0].city} - ${responseJS.photographers[0].name}</h1>`;
+    // Mettre dans ue fonction displayPogotographer(responseJS)
+
+    for (let i = 0; i <= responseJS.photographers.length - 1; i++) {
+      let name = responseJS.photographers[i].name;
+
+      const joinName = (str) => {
+        let newName = str.replaceAll("-", "").split(" ");
+        console.log(newName);
+        let [n, p] = newName;
+        return [...n].join("") + [...p].join("");
+      };
+
+      let namePhotographe = joinName(name);
+      console.log(namePhotographe);
+
+      const photographerSection = document.querySelector(
+        ".photographer_section"
+      );
+
+      photographerSection.innerHTML += /*html*/ `
+        <article aria-describedby="carte du photographe ${responseJS.photographers[i].name}">
+            <a href="/photographer.html?id=${responseJS.photographers[i].id}">
+                <img src="../../assets/photographers/Photographers ID Photos/${namePhotographe}.jpg"  alt="photo de ${responseJS.photographers[i].name}">
+                <h2>${responseJS.photographers[i].name}</h2>
+            </a>
+            <h3>${responseJS.photographers[i].city},${responseJS.photographers[i].country}</h3>
+            <p>${responseJS.photographers[i].tagline}</p>
+            <h4>${responseJS.photographers[i].price}€/jour</h4>
+        </article>`;
+    }
   } catch (error) {
     console.log(error, "erreur");
   }
 
   // et bien retourner le tableau photographers seulement une fois récupéré
   return {
-    photographers: [...photographers, ...photographers, ...photographers],
+    // photographers: [...photographers, ...photographers, ...photographers],
   };
 }
 
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,10 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// *----Récupération de l'ID ciblé
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+
+const param = urlSearchParams.get("id");
+console.log("id", param);
+
+// Localiser à l'objet window
