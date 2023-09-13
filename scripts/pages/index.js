cab36514eca4cf58bd2cd1b02d88f98626459b62
Flo-le-dev <f.gironde.dev@gmail.com>
2023-09-13 22:50:59 +0200

    P6

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -8,36 +8,8 @@ async function getPhotographers() {
 
     console.log(responseJS, "objet javascript");
 
-    // Mettre dans ue fonction displayPogotographer(responseJS)
-
-    for (let i = 0; i <= responseJS.photographers.length - 1; i++) {
-      let name = responseJS.photographers[i].name;
-
-      const joinName = (str) => {
-        let newName = str.replaceAll("-", "").split(" ");
-        console.log(newName);
-        let [n, p] = newName;
-        return [...n].join("") + [...p].join("");
-      };
-
-      let namePhotographe = joinName(name);
-      console.log(namePhotographe);
-
-      const photographerSection = document.querySelector(
-        ".photographer_section"
-      );
-
-      photographerSection.innerHTML += /*html*/ `
-        <article aria-describedby="carte du photographe ${responseJS.photographers[i].name}">
-            <a href="/photographer.html?id=${responseJS.photographers[i].id}">
-                <img src="../../assets/photographers/Photographers ID Photos/${namePhotographe}.jpg"  alt="photo de ${responseJS.photographers[i].name}">
-                <h2>${responseJS.photographers[i].name}</h2>
-            </a>
-            <h3>${responseJS.photographers[i].city},${responseJS.photographers[i].country}</h3>
-            <p>${responseJS.photographers[i].tagline}</p>
-            <h4>${responseJS.photographers[i].price}€/jour</h4>
-        </article>`;
-    }
+    // Mettre dans une fonction displayPhotographer(responseJS)
+    displayPhotographer(responseJS);
   } catch (error) {
     console.log(error, "erreur");
   }
@@ -47,6 +19,39 @@ async function getPhotographers() {
     // photographers: [...photographers, ...photographers, ...photographers],
   };
 }
+
+// ----------------------------------------------------------------------
+// Création d'une fonction displayPhotographer(responseJS)
+
+const displayPhotographer = (responseJS) => {
+  for (let i = 0; i <= responseJS.photographers.length - 1; i++) {
+    let name = responseJS.photographers[i].name;
+
+    // Fonction pour mettre le nom des photographes au bon format (txt collé) pour l'appel d'image
+    const joinName = (str) => {
+      let newName = str.replaceAll("-", "").split(" ");
+      console.log(newName);
+      let [n, p] = newName;
+      return [...n].join("") + [...p].join("");
+    };
+
+    let namePhotographe = joinName(name);
+    console.log(namePhotographe);
+
+    const photographerSection = document.querySelector(".photographer_section");
+
+    photographerSection.innerHTML += /*html*/ `
+          <article aria-describedby="carte du photographe ${responseJS.photographers[i].name}">
+              <a href="/photographer.html?id=${responseJS.photographers[i].id}">
+                  <img src="../../assets/photographers/Photographers ID Photos/${namePhotographe}.jpg"  alt="photo de ${responseJS.photographers[i].name}">
+                  <h2>${responseJS.photographers[i].name}</h2>
+              </a>
+              <h3>${responseJS.photographers[i].city},${responseJS.photographers[i].country}</h3>
+              <p>${responseJS.photographers[i].tagline}</p>
+              <h4>${responseJS.photographers[i].price}€/jour</h4>
+          </article>`;
+  }
+};
 
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,6 +5,64 @@
 const urlSearchParams = new URLSearchParams(window.location.search);
 
 const param = urlSearchParams.get("id");
-console.log("id", param);
+console.log("l'id est", param);
 
 // Localiser à l'objet window
+
+async function getPhotographers() {
+  try {
+    const responseJSON = await fetch("../../data/photographers.json");
+    // Code a exectué après reception de la réponse
+    // Conversion de la réponse au format javascript
+    const responseJS = await responseJSON.json();
+    // !let photographers = responseJS.photographers;
+
+    // Je crée un tableau vide qui va accueillir les 6 IDs grace à la boucle for
+    let IdPhotoTab = [];
+
+    // ----Boucle for qui parcour l'Objet responseJS.photographers
+    for (let i = 0; i < responseJS.photographers.length; i++) {
+      // J'ajoute les IDs à mon tableau
+      IdPhotoTab.push(responseJS.photographers[i].id);
+    }
+
+    // Fonction qui retourn true si les valeurs sont égales
+    const isCurrentID = (element) => {
+      return element === Number(param);
+    };
+
+    // FindIndex nous donnera l'index de l'élément correspond
+    let currentIdIndex = IdPhotoTab.findIndex(isCurrentID);
+
+    console.log(IdPhotoTab);
+    console.log(currentIdIndex);
+
+    const main = document.getElementById("main");
+    main.innerHTML = /*html*/ `<h2>${responseJS.photographers[currentIdIndex].name}</h2>`;
+  } catch (error) {
+    console.log(error, "erreur");
+  }
+
+  // et bien retourner le tableau photographers seulement une fois récupéré
+  //!   return {
+  //     photographers: responseJS.photographers,
+  //   };
+}
+
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
